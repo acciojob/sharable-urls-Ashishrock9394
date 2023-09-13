e0412919c1f1505da1f53ee8abd832344d46acde
Ashish Kumar <48367301+Ashishrock9394@users.noreply.github.com>
2023-09-13 17:54:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// your code here
+const form = document.querySelector("#form"); 
+  
+ form.addEventListener("submit",()=>{ 
+         event.preventDefault(); 
+         var url = "https://localhost:8080/"; 
+         var name = document.getElementById("name").value; 
+         var year = document.getElementById("year").value; 
+         let abc = document.querySelector("#url"); 
+  
+         abc.textContent = `${url}?${name!=""?`name=${name}&`:""}${year!=""?`year=${year}`:""}`; 
+ })
